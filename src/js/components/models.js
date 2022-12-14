a5b611f5d0f500b6d7856ded1cd8e404e5b5e874
obloomfield <obloomfield@brown.edu>
2022-12-14 16:14:45 -0500

    remove cork

--- src/js/components/models.js
+++ src/js/components/models.js
@@ -76,12 +76,12 @@ export function updateButton() {
 }
 
 function setObjShadow(obj) {
-  obj.traverse( function( node ) { 
-    if ( node instanceof THREE.Mesh ) { 
-      node.castShadow = true; 
+  obj.traverse(function (node) {
+    if (node instanceof THREE.Mesh) {
+      node.castShadow = true;
       node.receiveShadow = true;
-    } 
-  } );
+    }
+  });
 }
 
 export async function addModels() {
@@ -110,7 +110,6 @@ export async function addModels() {
   deskObj.translateY(-2475);
   deskObj.translateZ(-1000);
   deskObj.translateX(1000);
-
 
   // const desk = await load_model("public/models/simple_dirty_desk.glb", "desk");
   // desk.scale.set(2000,2000,2000);
@@ -151,7 +150,7 @@ export async function addModels() {
   cork.translateY(450);
   cork.translateX(300);
   cork.rotateZ(-Math.PI / 5.75);
-  SCENEDATA.add("cork", cork);
+  // SCENEDATA.add("cork", cork);
 
   const buttonChamfergeometry = new THREE.TorusGeometry(50, 15, 3, 100);
   const buttonChamfermaterial = new THREE.MeshStandardMaterial({
@@ -202,8 +201,8 @@ export async function addModels() {
 
   const floorGeometry = new THREE.BoxGeometry(6000, 500, 6000);
 
-  const floorMaterial = new THREE.MeshStandardMaterial({ 
-    color: 0xc3ada2, 
+  const floorMaterial = new THREE.MeshStandardMaterial({
+    color: 0xc3ada2,
   });
   const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 
